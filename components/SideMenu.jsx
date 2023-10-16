c16e2fad4b7e14c99a150693b9faea1a30d74727
mahat sumit <mahatsumit5@gmail.com>
2023-10-17 00:36:45 +1100

    responsive

--- components/SideMenu.jsx
+++ components/SideMenu.jsx
@@ -2,8 +2,7 @@
 import { useState, useEffect } from "react";
 import Image from "next/image";
 import { sidebarMenuLinks } from "@/constants/sidebarMenu";
-import { useRef } from "react";
-import { motion, useInView } from "framer-motion";
+import { AnimatePresence, motion } from "framer-motion";
 import Socialcons from "./social/Socialcons";
 import { useDispatch, useSelector } from "react-redux";
 import { toggleMenu } from "@/redux/useMenuSlice";
@@ -20,7 +19,6 @@ const SideMenu = () => {
   };
   const [scrollY, setScrollY] = useState(0);
   useEffect(() => {
-    dispatch(toggleMenu(true));
     const handleScroll = () => {
       setScrollY(window.scrollY);
     };
@@ -47,65 +45,75 @@ const SideMenu = () => {
       setActiveLink(sidebarMenuLinks[4]);
     }
   }, [scrollY]);
+  useEffect(() => {
+    if (window.innerWidth < 768) {
+      dispatch(toggleMenu(false));
+      return;
+    }
+    dispatch(toggleMenu(true));
+  }, [window.innerWidth]);
 
   return (
     <>
-      <motion.section
-        // initial={{ x: -120, y: "-100vh" }}
-        // animate={{ x: isOpen ? 0 : -120, y: isOpen ? 0 : "-100vh" }}
-        // initial={{ y: "-100vh", x: 0 }}
-        // animate={{ y: isOpen ? 0 : "-100vh" }}
-        animate={isOpen ? "open" : "closed"}
-        variants={variants}
-        transition={{ duration: 0.5 }}
-        className={`side-menu border-r ${
-          isOpen ? "max-lg:block" : "max-md:hidden"
-        }`}
-      >
-        <div className="flex w-full flex-1 flex-col gap-10 justify-between items-center">
-          <div className="flex justify-center items-center flex-col gap-2 rounded-full ">
-            <Image
-              src="/assets/hero.png"
-              width={100}
-              height={100}
-              alt="sumit"
-            />
-            <p className="font-bold text-dark-blue">Sumit Mahat </p>
-          </div>
-
-          <div>
-            {sidebarMenuLinks.map((link) => {
-              const isActive = activeLink === link;
-              return (
-                <motion.a
-                  initial={{ x: "-100vw" }}
-                  animate={{ x: 0 }}
-                  transition={{ duration: link.duration }}
-                  whileHover={{ scale: 1.2 }}
-                  key={link.label}
-                  href={`${link.route}`}
-                  className={`relative flex justify-center items-center rounded-lg p-3 ${
-                    isActive ? "bg-dark-red text-white" : ""
-                  }`}
-                  onClick={() => {
-                    handleLinkclick(link);
-                  }}
-                >
-                  <p> {link.label}</p>
-                </motion.a>
-              );
-            })}
-          </div>
-          <div className="flex items-center flex-col justify-center text-center">
-            <div className="flex flex-col lg:flex-row items-center gap-4 mb-3">
-              <Socialcons />
+      <AnimatePresence>
+        <motion.section
+          // initial={{ x: -120, y: "-100vh" }}
+          // animate={{ x: isOpen ? 0 : -120, y: isOpen ? 0 : "-100vh" }}
+          initial={{ y: "-100vh", x: -120, opacity: 0 }}
+          animate={{
+            y: isOpen ? 0 : "-100vh",
+            x: isOpen ? 0 : -120,
+            opacity: isOpen ? 1 : 0,
+          }}
+          variants={variants}
+          transition={{ duration: 0.5 }}
+          className={`side-menu border-r `}
+        >
+          <div className="flex w-full flex-1 flex-col gap-10 justify-between items-center">
+            <div className="flex justify-center items-center flex-col gap-2 rounded-full ">
+              <Image
+                src="/assets/hero.png"
+                width={100}
+                height={100}
+                alt="sumit"
+              />
+              <p className="font-bold text-dark-blue">Sumit Mahat </p>
             </div>
-            <p className="max-md:hidden">
-              Copyright 2023 Sumit Mahat.All rights reserved
-            </p>
+
+            <div>
+              {sidebarMenuLinks.map((link) => {
+                const isActive = activeLink === link;
+                return (
+                  <motion.a
+                    initial={{ x: "-100vw" }}
+                    animate={{ x: 0 }}
+                    transition={{ duration: link.duration }}
+                    whileHover={{ scale: 1.2 }}
+                    key={link.label}
+                    href={`${link.route}`}
+                    className={`relative flex justify-center items-center rounded-lg p-3 ${
+                      isActive ? "bg-dark-red text-white" : ""
+                    }`}
+                    onClick={() => {
+                      handleLinkclick(link);
+                    }}
+                  >
+                    <p> {link.label}</p>
+                  </motion.a>
+                );
+              })}
+            </div>
+            <div className="flex items-center flex-col justify-center text-center">
+              <div className="flex flex-col lg:flex-row items-center gap-4 mb-3">
+                <Socialcons />
+              </div>
+              <p className="max-md:hidden">
+                Copyright 2023 Sumit Mahat.All rights reserved
+              </p>
+            </div>
           </div>
-        </div>
-      </motion.section>
+        </motion.section>
+      </AnimatePresence>
     </>
   );
 };
