38919f7cbf795891ac5998029cba73718dcf7494
Sumit Mahat <mahatsumit5@gmail.com>
2024-02-22 00:18:42 +1100

    api creatd for chat bot

---
+++ app/api/chatbot/route.js
@@ -0,0 +1,42 @@
+import { NextResponse } from "next/server";
+import fs from "fs";
+import path from "path";
+import { OpenAI } from "openai";
+export async function POST(req) {
+  const openai = new OpenAI({ apiKey: process.env.OPENAI_API_KEY });
+  const pathName = path.resolve();
+  try {
+    const { prompt } = await req.json();
+
+    const myFile = fs.readFileSync(pathName + "/sumit.txt", "utf8");
+    const completion = await openai.chat.completions.create({
+      model: "gpt-3.5-turbo",
+      messages: [
+        {
+          role: "system",
+          content: ` You are my assitant. That answer question for vistors who vists my website. Address me as MR Sumit.${myFile}`,
+        },
+        { role: "user", content: "Who is sumit Mahat." },
+        {
+          role: "assistant",
+          content:
+            "mr sumit is a Experienced Web Developer looking forward to obtaining a full-time job working as a Web Developer to develop, maintain and design websites.  Hands on experience in creating and developing multiple web applications using various web technologies.",
+        },
+        {
+          role: "user",
+          content: prompt,
+        },
+      ],
+    });
+    const { message } = completion.choices[0];
+    return NextResponse.json({
+      status: "success",
+      message,
+    });
+  } catch (error) {
+    return NextResponse.json({
+      status: "error",
+      message: error.message,
+    });
+  }
+}
